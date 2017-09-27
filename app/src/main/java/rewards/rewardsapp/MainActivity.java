297ef98838c7d49deb4d51a46a57917010f388d3
Andrew Miller <mill6841@d.umn.edu>
2017-09-27 01:13:53 -0500

    Added earn points activity

--- app/src/main/java/rewards/rewardsapp/MainActivity.java
+++ app/src/main/java/rewards/rewardsapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
 //        if (id == R.id.action_settings) {
 //            return true;
 //        }
-
         return super.onOptionsItemSelected(item);
     }
+
+    void onClick
 }
